5577699b5294c8e57593798af63c11d169dee539
Yuriy <eurweb@gmail.com>
2021-03-21 16:45:26 +0200

    validation
    validation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,17 @@ use CodeIgniter\Model;
 
 class Post extends Model
 {
+
+	protected $validationRules    = [
+        'name'     => 'alpha_space|min_length[3]|max_length[50]',
+        'email'    => 'valid_email|is_unique[posts.email]'
+    ];
+
     protected $table = 'posts'; 
     protected $primaryKey = 'id';
 
     protected $returnType = 'array';
 
-    protected $allowedFields = ['name', 'email'];
+    protected $allowedFields = ['name', 'email','image'];
 
 }
