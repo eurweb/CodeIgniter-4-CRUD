47857199f07374d846bce0a8d1162dd41da04825
Yuriy <eurweb@gmail.com>
2021-03-21 16:45:04 +0200

    Add files via upload

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,37 +1,31 @@
 <!doctype html>
 <html>
 <head>
-  <title>CRUD  CodeIgniter 4</title>
+  <title>CodeIgniter 4</title>
 
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-  <style type="text/css">
-  .mb20{
-     margin-bottom: 20px;
-  }
-  .actionbutton{
-     width: 100%;
-     height: 55px;
-  }
-  .errors{
-     color: red;
-  }
-  </style>
+  
 </head>
 <body>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-         <?= $this->renderSection('content') ?>
-      </div>
-    </div>
 
+<header>
 
-     <div class="row">
-      <div class="col-md-12"> alll view 
-         <?= $this->renderSection('footer') ?>
-      </div>
-    </div>
+<nav class="navbar navbar-light navbar-expand-lg">
+<ul class="navbar-nav">
+           
+<li class="menu-item"><a class="nav-link" href="/">Home</a></li>
+<li class="menu-item"><a class="nav-link" href="https://codeigniter4.github.io/userguide/" target="_blank">Docs</a>
+<li class="menu-item"><a class="nav-link" href="/users/">Users</a></li>
+   
+</ul>
+</nav>
+
+</header>
+
+<div class="container"> 
+<?= $content ?>     
+</div>
 
   </div>
 </body>
